215b7eb1f9caabef2aafeeecc54ca1c0c3595458
KathyOlsen <katherinekolsen@verizon.net>
2019-10-15 10:46:25 -0400

    SpringBoot_405

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,10 +1,18 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    UserRepository userRepository;
+
     @RequestMapping("/")
     public String index(){
         return "index";
@@ -16,7 +24,9 @@ public class HomeController {
     }
 
     @RequestMapping("/secure")
-    public String secure(){
+    public String secure(Principal principal, Model model){
+        String username = principal.getName();
+        model.addAttribute("user",userRepository.findByUsername(username));
         return "secure";
     }
 }
